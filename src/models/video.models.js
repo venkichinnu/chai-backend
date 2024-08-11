6cb956423325e7bd0b6f96aae1ba0c8720986618
venkichinnu <venki.kethavath007@gmail.com>
2024-08-11 13:42:52 +0530

    add user and video models

---
+++ src/models/video.models.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: [true, "Video file is required"],
+    },
+    thumbNail: {
+      type: String,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+export const Video = mongoose.model("Video", videoSchema);
